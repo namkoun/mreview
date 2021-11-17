916d8831508bce6c2d013812dd13bb85fb396013
namkoun <skarud0328@gmail.com>
2021-11-17 16:48:56 +0900

    three day commit

--- src/main/java/oeg/zerock/mreview/controller/MovieController.java
+++ src/main/java/oeg/zerock/mreview/controller/MovieController.java
@@ -1,19 +1,37 @@
 package oeg.zerock.mreview.controller;
 
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import oeg.zerock.mreview.dto.MovieDTO;
+import oeg.zerock.mreview.service.MovieService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/movie")
 @Log4j2
+@RequiredArgsConstructor
 public class MovieController {
 
+    private final MovieService movieService;
 
     @GetMapping("/register")
     public void register(){
-
     }
+
+    @PostMapping("/register")
+    public String regster(MovieDTO movieDTO, RedirectAttributes redirectAttributes){
+        log.info("movieDTO: "+movieDTO);
+
+        Long mno = movieService.register(movieDTO);
+
+        redirectAttributes.addFlashAttribute("msg", mno);
+
+        return "redirect:/movie/list";
+    }
+
 }
